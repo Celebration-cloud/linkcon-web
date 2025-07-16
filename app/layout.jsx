82d591dd278a5fcfa71f12f4b0f4b19e5bc04846
Celebration-cloud <ojinguluc@gmail.com>
2025-07-16 08:35:25 +0100

    adsense

--- app/layout.jsx
+++ app/layout.jsx
@@ -145,6 +145,11 @@ export default async function RootLayout({ children }) {
             noModule
             src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
           />
+          <script
+            async
+            crossOrigin="anonymous"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7517532434811402"
+          />
 
           {/* Vercel Analytics */}
           <Analytics />
